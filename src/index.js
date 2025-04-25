7df19bdeee0292cd595fc5f96d55c102610e7539
晚冬老叔 <58387690+wdlaoshu@users.noreply.github.com>
2025-04-25 17:08:11 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,260 @@
+import dayjs from 'dayjs'
+import { Router } from 'itty-router'
+import Cookies from 'cookie'
+import jwt from '@tsndr/cloudflare-worker-jwt'
+import { queryNote, MD5, checkAuth, genRandomStr, returnPage, returnJSON, saltPw, getI18n } from './helper'
+import { SECRET } from './constant'
+import { saltPw } from './helper'
+
+// init
+const router = Router()
+
+// 全局认证中间件
+router.all('*', async (request, env) => {
+  const url = new URL(request.url)
+  
+  // 排除分享路由和管理登录接口
+  if(url.pathname.startsWith('/share') || url.pathname === '/admin/login') {
+    return
+  }
+
+  // 验证管理员Cookie
+  const cookie = Cookies.parse(request.headers.get('Cookie') || '')
+  const validAdmin = await checkAdminAuth(cookie)
+  
+  if(!validAdmin) {
+    return Response.redirect(url.origin + '/admin', 302)
+  }
+})
+
+router.get('/', ({ url }) => {
+    const newHash = genRandomStr(3)
+    // redirect to new page
+    return Response.redirect(`${url}${newHash}`, 302)
+})
+
+router.get('/share/:md5', async (request) => {
+    const lang = getI18n(request)
+    const { md5 } = request.params
+    const path = await SHARE.get(md5)
+
+    if (!!path) {
+        const { value, metadata } = await queryNote(path)
+
+        return returnPage('Share', {
+            lang,
+            title: decodeURIComponent(path),
+            content: value,
+            ext: metadata,
+        })
+    }
+
+    return returnPage('Page404', { lang, title: '404' })
+})
+
+router.get('/:path', async (request) => {
+    const lang = getI18n(request)
+
+    const { path } = request.params
+    const title = decodeURIComponent(path)
+
+    const cookie = Cookies.parse(request.headers.get('Cookie') || '')
+
+    const { value, metadata } = await queryNote(path)
+
+    if (!metadata.pw) {
+        return returnPage('Edit', {
+            lang,
+            title,
+            content: value,
+            ext: metadata,
+        })
+    }
+
+    const valid = await checkAuth(cookie, path)
+    if (valid) {
+        return returnPage('Edit', {
+            lang,
+            title,
+            content: value,
+            ext: metadata,
+        })
+    }
+
+    return returnPage('NeedPasswd', { lang, title })
+})
+
+// 管理页面路由
+router.get('/admin', (request) => {
+  const lang = getI18n(request)
+  return returnPage('Admin', { lang })
+})
+
+// 管理员登录接口
+router.post('/admin/login', async request => {
+  if (request.headers.get('Content-Type') === 'application/json') {
+    const { password } = await request.json()
+    const hashedPw = await saltPw(password)
+    
+    const storedPw = await ADMIN.get('password')
+    
+    if(hashedPw === storedPw) {
+      const token = await jwt.sign({ admin: true }, SECRET)
+      return returnJSON(0, {
+        redirect: '/'
+      }, {
+        'Set-Cookie': Cookies.serialize('admin', token, {
+          path: '/',
+          expires: dayjs().add(1, 'day').toDate(),
+          httpOnly: true
+        })
+      })
+    }
+  }
+  return returnJSON(10005, '管理员密码错误')
+})
+
+router.post('/:path/auth', async request => {
+    const { path } = request.params
+    if (request.headers.get('Content-Type') === 'application/json') {
+        const { passwd } = await request.json()
+
+        const { metadata } = await queryNote(path)
+
+        if (metadata.pw) {
+            const storePw = await saltPw(passwd)
+
+            if (metadata.pw === storePw) {
+                const token = await jwt.sign({ path }, SECRET)
+                return returnJSON(0, {
+                    refresh: true,
+                }, {
+                    'Set-Cookie': Cookies.serialize('auth', token, {
+                        path: `/${path}`,
+                        expires: dayjs().add(7, 'day').toDate(),
+                        httpOnly: true,
+                    })
+                })
+            }
+        }
+    }
+
+    return returnJSON(10002, 'Password auth failed!')
+})
+
+router.post('/:path/pw', async request => {
+    const { path } = request.params
+    if (request.headers.get('Content-Type') === 'application/json') {
+        const cookie = Cookies.parse(request.headers.get('Cookie') || '')
+        const { passwd } = await request.json()
+
+        const { value, metadata } = await queryNote(path)
+        const valid = await checkAuth(cookie, path)
+        
+        if (!metadata.pw || valid) {
+            const pw = passwd ? await saltPw(passwd) : undefined
+            try {
+                await NOTES.put(path, value, {
+                    metadata: {
+                        ...metadata,
+                        pw,
+                    },
+                })
+    
+                return returnJSON(0, null, {
+                    'Set-Cookie': Cookies.serialize('auth', '', {
+                        path: `/${path}`,
+                        expires: dayjs().subtract(100, 'day').toDate(),
+                        httpOnly: true,
+                    })
+                })
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        return returnJSON(10003, 'Password setting failed!')
+    }
+})
+
+router.post('/:path/setting', async request => {
+    const { path } = request.params
+    if (request.headers.get('Content-Type') === 'application/json') {
+        const cookie = Cookies.parse(request.headers.get('Cookie') || '')
+        const { mode, share } = await request.json()
+
+        const { value, metadata } = await queryNote(path)
+        const valid = await checkAuth(cookie, path)
+        
+        if (!metadata.pw || valid) {
+            try {
+                await NOTES.put(path, value, {
+                    metadata: {
+                        ...metadata,
+                        ...mode !== undefined && { mode },
+                        ...share !== undefined && { share },
+                    },
+                })
+
+                const md5 = await MD5(path)
+                if (share) {
+                    await SHARE.put(md5, path)
+                    return returnJSON(0, md5)
+                }
+                if (share === false) {
+                    await SHARE.delete(md5)
+                }
+
+
+                return returnJSON(0)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        return returnJSON(10004, 'Update Setting failed!')
+    }
+})
+
+router.post('/:path', async request => {
+    const { path } = request.params
+    const { value, metadata } = await queryNote(path)
+
+    const cookie = Cookies.parse(request.headers.get('Cookie') || '')
+    const valid = await checkAuth(cookie, path)
+
+    if (!metadata.pw || valid) {
+        // OK
+    } else {
+        return returnJSON(10002, 'Password auth failed! Try refreshing this page if you had just set a password.')
+    }
+
+    const formData = await request.formData();
+    const content = formData.get('t')
+
+    // const { metadata } = await queryNote(path)
+
+    try {
+        await NOTES.put(path, content, {
+            metadata: {
+                ...metadata,
+                updateAt: dayjs().unix(),
+            },
+        })
+
+        return returnJSON(0)
+    } catch (error) {
+        console.error(error)
+    }
+
+    return returnJSON(10001, 'KV insert fail!')
+})
+
+router.all('*', (request) => {
+    const lang = getI18n(request)
+    returnPage('Page404', { lang, title: '404' })
+})
+
+addEventListener('fetch', event => {
+    event.respondWith(router.handle(event.request))
+})
---
+++ static/js/app.js
@@ -0,0 +1,235 @@
+
+const DEFAULT_LANG = 'en'
+const SUPPORTED_LANG = {
+    'en': {
+        err: 'Error',
+        pepw: 'Please enter password.',
+        pwcnbe: 'Password is empty!',
+        enpw: 'Enter a new password(Keeping it empty will remove the current password)',
+        pwss: 'Password set successfully.',
+        pwrs: 'Password removed successfully.',
+        cpys: 'Copied!',
+    },
+    'zh': {
+        err: '出错了',
+        pepw: '请输入密码',
+        pwcnbe: '密码不能为空！',
+        enpw: '输入新密码（留空可清除当前密码）',
+        pwss: '密码设置成功！',
+        pwrs: '密码清除成功！',
+        cpys: '已复制',
+    }
+}
+
+const getI18n = key => {
+    const userLang = (navigator.language || navigator.userLanguage || DEFAULT_LANG).split('-')[0]
+    const targetLang = Object.keys(SUPPORTED_LANG).find(l => l === userLang) || DEFAULT_LANG
+    return SUPPORTED_LANG[targetLang][key]
+}
+
+const errHandle = (err) => {
+    alert(`${getI18n('err')}: ${err}`)
+}
+
+const throttle = (func, delay) => {
+    let tid = null
+
+    return (...arg) => {
+        if (tid) return;
+
+        tid = setTimeout(() => {
+            func(...arg)
+            tid = null
+        }, delay)
+    }
+}
+
+const passwdPrompt = () => {
+    const passwd = window.prompt(getI18n('pepw'))
+    if (passwd == null) return;
+
+    if (!passwd.trim()) {
+        alert(getI18n('pwcnbe'))
+    }
+    const path = location.pathname
+    window.fetch(`${path}/auth`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            passwd,
+        }),
+    })
+        .then(res => res.json())
+        .then(res => {
+            if (res.err !== 0) {
+                return errHandle(res.msg)
+            }
+            if (res.data.refresh) {
+                window.location.reload()
+            }
+        })
+        .catch(err => errHandle(err))
+}
+
+const renderPlain = (node, text) => {
+    if (node) {
+        node.innerHTML = DOMPurify.sanitize(text)
+    }
+}
+
+const renderMarkdown = (node, text) => {
+    if (node) {
+        const parseText = marked.parse(text)
+        node.innerHTML = DOMPurify.sanitize(parseText)
+    }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    const $textarea = document.querySelector('#contents')
+    const $loading = document.querySelector('#loading')
+    const $pwBtn = document.querySelector('.opt-pw')
+    const $modeBtn = document.querySelector('.opt-mode > input')
+    const $shareBtn = document.querySelector('.opt-share > input')
+    const $previewPlain = document.querySelector('#preview-plain')
+    const $previewMd = document.querySelector('#preview-md')
+    const $shareModal = document.querySelector('.share-modal')
+    const $closeBtn = document.querySelector('.share-modal .close-btn')
+    const $copyBtn = document.querySelector('.share-modal .opt-button')
+    const $shareInput = document.querySelector('.share-modal input')
+
+    renderPlain($previewPlain, $textarea.value)
+    renderMarkdown($previewMd, $textarea.value)
+
+    if ($textarea) {
+        $textarea.oninput = throttle(function () {
+            renderMarkdown($previewMd, $textarea.value)
+
+            $loading.style.display = 'inline-block'
+            const data = {
+                t: $textarea.value,
+            }
+
+            window.fetch('', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams(data),
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if (res.err !== 0) {
+                        errHandle(res.msg)
+                    }
+                })
+                .catch(err => errHandle(err))
+                .finally(() => {
+                    $loading.style.display = 'none'
+                })
+        }, 1000)
+    }
+
+    if ($pwBtn) {
+        $pwBtn.onclick = function () {
+            const passwd = window.prompt(getI18n('enpw'))
+            if (passwd == null) return;
+
+            const path = window.location.pathname
+            window.fetch(`${path}/pw`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    passwd: passwd.trim(),
+                }),
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if (res.err !== 0) {
+                        return errHandle(res.msg)
+                    }
+                    alert(passwd ? getI18n('pwss') : getI18n('pwrs'))
+                })
+                .catch(err => errHandle(err))
+        }
+    }
+
+    if ($modeBtn) {
+        $modeBtn.onclick = function (e) {
+            const isMd = e.target.checked
+            const path = window.location.pathname
+            window.fetch(`${path}/setting`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    mode: isMd ? 'md' : 'plain',
+                }),
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if (res.err !== 0) {
+                        return errHandle(res.msg)
+                    }
+
+                    window.location.reload()
+                })
+                .catch(err => errHandle(err))
+        }
+    }
+
+    if ($shareBtn) {
+        $shareBtn.onclick = function (e) {
+            const isShare = e.target.checked
+            const path = window.location.pathname
+            window.fetch(`${path}/setting`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    share: isShare,
+                }),
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if (res.err !== 0) {
+                        return errHandle(res.msg)
+                    }
+
+                    if (isShare) {
+                        const origin = window.location.origin
+                        const url = `${origin}/share/${res.data}`
+                        // show modal
+                        $shareInput.value = url
+                        $shareModal.style.display = 'block'
+                    }
+                })
+                .catch(err => errHandle(err))
+        }
+    }
+
+    if ($shareModal) {
+        $closeBtn.onclick = function () {
+            $shareModal.style.display = 'none'
+
+        }
+        $copyBtn.onclick = function () {
+            clipboardCopy($shareInput.value)
+            const originText = $copyBtn.innerHTML
+            const originColor = $copyBtn.style.background
+            $copyBtn.innerHTML = getI18n('cpys')
+            $copyBtn.style.background = 'orange'
+            window.setTimeout(() => {
+                $shareModal.style.display = 'none'
+                $copyBtn.innerHTML = originText
+                $copyBtn.style.background = originColor
+            }, 1500)
+        }
+    }
+
+})
